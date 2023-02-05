1db45bd73e48cf8eb3adb024786244b849424ec7
Edgar Messia <ynfantes@gmail.com>
2023-02-05 13:16:37 +0100

    fix: :bug: No se están mostrando las imágenes al registrar una propiedad
    En el módulo inmobiliario, se registra una propiedad y se cargan las imágnes. El proceso deice finalizco con éxito, pero las imagenes no se cargan en servidor ni se muestran en el detalle de la propiedad

--- administracion/index.php
+++ administracion/index.php
@@ -5,7 +5,7 @@ $accion = isset($_GET['accion'])?$_GET['accion']:"";
 $publicaciones = new publicaciones();
 switch ($accion) {
     
-    // <editor-fold defaultstate="collapsed" desc="login">
+    
     case "login":
         $administrador = Array();
         $login = new administrador();
@@ -14,36 +14,30 @@ switch ($accion) {
         }
         echo $twig->render('administracion/index.html.twig', Array("administrador" => $administrador));
         break; 
-    // </editor-fold>
 
-    // <editor-fold defaultstate="collapsed" desc="inactivar">
     case "inactivar":
         if (isset($_GET['id'])) {
-            $resultado = $publicaciones->actualizar($_GET['id'], Array("inactivo" => 1));
-            if ($resultado['suceed']) {
-                $resultado['mensaje'] = "(".$resultado['stats']['affected_rows'].") Publicación inactivada) con éxito.";
+            $result = $publicaciones->actualizar($_GET['id'], Array("inactivo" => 1));
+            if ($result['suceed']) {
+                $result['mensaje'] = "(".$result['stats']['affected_rows'].") Publicación inactivada) con éxito.";
             } else {
-                $resultado['mensaje'] = "Ups! ocurrió un error al tratar de inactivar la publicación.";
+                $result['mensaje'] = "Ups! ocurrió un error al tratar de inactivar la publicación.";
             }
             
         }
         $listado = $publicaciones->obtenerPublicaciones();
         echo $twig->render('administracion/lista-publicaciones.html.twig', Array(
-            "listado" => $listado['data'],"resultado"=>$resultado
+            "listado" => $listado['data'],"resultado"=>$result
         ));
         break;
-    // </editor-fold>
      
-    // <editor-fold defaultstate="collapsed" desc="publicaciones">
     case "publicaciones":
         $listado = $publicaciones->obtenerPublicaciones();
         echo $twig->render('administracion/lista-publicaciones.html.twig', Array(
             "listado" => $listado['data']
         ));
         break; 
-    // </editor-fold>
 
-    // <editor-fold defaultstate="collapsed" desc="publicar">
     case "publicar":
         $db = new db();
         $municipios = $db->dame_query("select * from municipios where id > 1 order by descripcion ");
@@ -53,15 +47,20 @@ switch ($accion) {
             "municipios" => $municipios['data'],
             "tipo" => $tipo['data'],
             "operacion" => $operacion['data']));
-        break; // </editor-fold>
+        break; 
 
-    // <editor-fold defaultstate="collapsed" desc="guardar">
     case "guardar":
-        $dir = "images/prv/estate/";
-        $db = new db();
-        $resultado = Array();
-        $data = $_POST;
-
+        $dir    = "images/prv/estate/";
+        $db     = new db();
+        $result = [];
+        $data   = $_POST;
+        if(!empty($_FILES)){
+            echo 'vienen files';
+            $route = $dir . basename($FILES['file']['name']);
+            die($route);
+            move_uploaded_file($_FILES['file']['tmp'],$route);
+        }
+        die('fin');
         if (isset($data['imagen'])) {
             $imagenes = $data['imagen'];
             unset($data['imagen']);
@@ -70,29 +69,29 @@ switch ($accion) {
         if (isset($data['editar']) && $data['editar'] == 'editar') {
             unset($data['editar']);
             $id = $data['id'];
-            $resultado = $publicaciones->actualizar($id, $data);
-            if ($resultado['suceed']) {
-                $resultado['mensaje'] = 'Publicación actualizada con éxito!';
+            $result = $publicaciones->actualizar($id, $data);
+            if ($result['suceed']) {
+                $result['mensaje'] = 'Publicación actualizada con éxito!';
             } else {
-                $resultado['mensaje'] = 'Ups! Ocurrió un error durante el proceso. No se puedo actualizar la información';
+                $result['mensaje'] = 'Ups! Ocurrió un error durante el proceso. No se puedo actualizar la información';
             }
             $db->delete("inmobiliaria_img", Array("id_inmobiliaria_publicacion" => $id));
         } else {
             // guardar
-            $resultado = $publicaciones->insertar($data);
-            if ($resultado['suceed']) {
-                $id = $resultado['insert_id'];
-                $resultado['mensaje'] = 'Publicación guardada con éxito';
+            $result = $publicaciones->insertar($data);
+            if ($result['suceed']) {
+                $id = $result['insert_id'];
+                $result['mensaje'] = 'Publicación guardada con éxito';
             } else {
-                $resultado['mensaje'] = 'Ups! Ocurrió un error durante el proceso. No se puedo guardar la información';
+                $result['mensaje'] = 'Ups! Ocurrió un error durante el proceso. No se puedo guardar la información';
             }
         }
-        if ($resultado['suceed']) {
+        if ($result['suceed']) {
             if (isset($imagenes)) {
                 $i = 0;
                 foreach ($imagenes as $imagen) {
                     if ($i == 0) {
-                        $resultado = $publicaciones->actualizar($id, Array("imagen" => $dir . $imagen));
+                        $result = $publicaciones->actualizar($id, Array("imagen" => $dir . $imagen));
                     } else {
                         $db->insert("inmobiliaria_img", Array("id_inmobiliaria_publicacion" => $id, "imagen" => $dir . $imagen));
                     }
@@ -101,33 +100,31 @@ switch ($accion) {
             }
         }
 
-        $propiedad = $publicaciones->ver($id);
-
+        $propiedad  = $publicaciones->ver($id);
         $municipios = $db->dame_query("select * from municipios where id > 1 order by descripcion ");
-        $tipo = $db->dame_query("select * from inmobiliaria_tipo where id > 1 order by descripcion");
-        $operacion = $db->dame_query("select * from operaciones where id > 1 order by descripcion");
+        $tipo       = $db->dame_query("select * from inmobiliaria_tipo where id > 1 order by descripcion");
+        $operacion  = $db->dame_query("select * from operaciones where id > 1 order by descripcion");
+
+        $opciones = [
+                'municipios' => $municipios['data'],
+                'tipo'       => $tipo['data'],
+                'operacion'  => $operacion['data'],
+                'propiedad'  => $propiedad['data'],
+                'accion'     => 'editar',
+                'resultado'  => $result,
+            ];
+        echo $twig->render('administracion/registrar-propiedad.html.twig', $opciones);
 
 
-        echo $twig->render('administracion/registrar-propiedad.html.twig', Array(
-            "municipios" => $municipios['data'],
-            "tipo" => $tipo['data'],
-            "operacion" => $operacion['data'],
-            "propiedad" => $propiedad['data'],
-            "accion" => "editar",
-            "resultado" => $resultado));
-
-
-        break; // </editor-fold>
+        break; 
     
-    // <editor-fold defaultstate="collapsed" desc="editar">
     case "editar":
         $db = new db();
         
         $municipios = $db->dame_query("select * from municipios where id > 1 order by descripcion ");
-        $tipo = $db->dame_query("select * from inmobiliaria_tipo where id > 1 order by descripcion");
-        $operacion = $db->dame_query("select * from operaciones where id > 1 order by descripcion");
-
-        $propiedad = $publicaciones->ver($_GET['id']);
+        $tipo       = $db->dame_query("select * from inmobiliaria_tipo where id > 1 order by descripcion");
+        $operacion  = $db->dame_query("select * from operaciones where id > 1 order by descripcion");
+        $propiedad  = $publicaciones->ver($_GET['id']);
 
         if ($propiedad['suceed'] && count($propiedad['data']) > 0) {
             for ($index = 0; $index < count($propiedad['data']); $index++) {
@@ -136,17 +133,15 @@ switch ($accion) {
             }
             $publicaciones->actualizar($_GET['id'], Array("visto" => $propiedad['data'][0]['visto'] + 1));
         }
-
-
-        echo $twig->render('administracion/registrar-propiedad.html.twig', Array(
-            "municipios" => $municipios['data'],
-            "tipo" => $tipo['data'],
-            "operacion" => $operacion['data'],
-            "propiedad" => $propiedad['data'],
-            "accion" => "editar"));
-
-
-        break; // </editor-fold>
+        $opciones = [
+            'municipios'  => $municipios['data'],
+            'tipo'        => $tipo['data'],
+            'operacion'   => $operacion['data'],
+            'propiedad'   => $propiedad['data'],
+            'accion'      => "editar"
+        ];
+        echo $twig->render('administracion/registrar-propiedad.html.twig', $opciones);
+        break; 
 
     default :
         echo $twig->render('administracion/index.html.twig');
